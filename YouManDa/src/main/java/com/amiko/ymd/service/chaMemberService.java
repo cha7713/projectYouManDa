29154eceb480335f975be4b1a332c143ae0b43a3
cha7713 <cha7713@gmail.com>
2019-09-18 10:50:22 +0900

    11

--- YouManDa/src/main/java/com/amiko/ymd/service/chaMemberService.java
+++ YouManDa/src/main/java/com/amiko/ymd/service/chaMemberService.java
@@ -3,38 +3,31 @@ package com.amiko.ymd.service;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.amiko.ymd.dao.chaMemberDao;
+
 /**
  * Handles requests for the application home page.
  */
-@Controller
+@Service
 public class chaMemberService {
-	// 11111
-	private static final Logger logger = LoggerFactory.getLogger(chaMemberService.class);
-	// real121212225555eee
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	@Autowired 
+	chaMemberDao dao;
+	
+	 public void join(Map<String, Object> map) {
+		 
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
 	}
 	
 }
