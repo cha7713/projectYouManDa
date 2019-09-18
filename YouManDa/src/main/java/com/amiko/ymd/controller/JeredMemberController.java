859b07dfd2106a8229d70ae3cdafe210e760c387
Jeredk <qpfmsktm9@gmail.com>
2019-09-18 10:20:50 +0900

    청솔이꺼

---
+++ YouManDa/src/main/java/com/amiko/ymd/controller/JeredMemberController.java
@@ -0,0 +1,40 @@
+package com.amiko.ymd.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class JeredMemberController {
+	// 11111
+	private static final Logger logger = LoggerFactory.getLogger(JeredMemberController.class);
+	// real121212225555eee
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+}
