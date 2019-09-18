1dcd66ab21e24507944c2be88025745b9951a0dc
cyuyoung <fghjkl2714@naver.com>
2019-09-18 18:35:46 +0900

    홈 디자인

--- YouManDa/src/main/java/com/amiko/ymd/controller/U0MemberController.java
+++ YouManDa/src/main/java/com/amiko/ymd/controller/U0MemberController.java
@@ -17,6 +17,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class U0MemberController {
 
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index(Locale locale, Model model) {
+
+		return "index";
+	}
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 
